8941c88bdbffc999048aea5da4cd6d822af3848d
AntonX Github <antonxgithub@gmail.com>
2012-05-05 22:15:29 -0400

    tuning of smartassV2 governor (cherry picked from commit 3680873ac30789b98f4fb5758bbad3df8633531c)
    Signed-off-by: Bryan Hundven <bryanhundven@gmail.com>

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -68,13 +68,13 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP (200*1000)
+#define DEFAULT_RAMP_DOWN_STEP (300*1000)
 static unsigned int ramp_down_step;
 
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 50
+#define DEFAULT_MAX_CPU_LOAD 55
 static unsigned long max_cpu_load;
 
 /*
@@ -101,7 +101,7 @@ static unsigned long down_rate_us;
  * The frequency to set when waking up from sleep.
  * When sleep_ideal_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ (1000*1000)
+#define DEFAULT_SLEEP_WAKEUP_FREQ (800*1000)
 static unsigned int sleep_wakeup_freq;
 
 /*
